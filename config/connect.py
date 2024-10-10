fb0fea2ecf62af9a4e9bdacd834827dc22318d92
reuven <r0583247229@gmail.com>
2024-10-10 14:17:44 +0300

    add func init database

--- config/connect.py
+++ config/connect.py
@@ -1,4 +1,14 @@
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb://172.22.88.20:27017')
+client = MongoClient('mongodb://localhost:27017')
+
+accidents_data_db = client['accidents_data']
+
+accidents = accidents_data_db['accidents']
+areas = accidents_data_db['areas']
+days = accidents_data_db['days']
+weeks = accidents_data_db['weeks']
+months = accidents_data_db['months']
+reasons = accidents_data_db['reasons']
+injuries = accidents_data_db['injuries']
